3ae3807b3112077feeb1522eccaeafe335329e9a
Luis Sousa <luispsousa@ua.pt>
2017-09-19 13:43:55 +0100

    Emit newMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ let socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'degue',
-    text: 'Hey. This is diji'
-  });
 });
 
 socket.on('disconnect', function () {
